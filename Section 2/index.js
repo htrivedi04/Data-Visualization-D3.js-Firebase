9d808a8549e94cec05e2e4102445c22d9954a267
htrivedi04 <h.trivedi04@gmail.com>
2019-07-03 12:41:32 +1000

    Adding elements in D3.js

---
+++ Section 2/index.js
@@ -0,0 +1,37 @@
+// Grab element from DOM in regular JS
+// Use querySelector to grab just one 
+const a  = document.querySelectorAll('div');
+// Grab element from DOM in D3.js
+// Use select to grab just one
+const b = d3.selectAll('div');
+
+const canvas = d3.select(".canvas");
+
+// Append an SVG tag to the 'canvas' div
+const svg = canvas.append('svg');
+// Adding attributes to SVG
+svg.attr('height', 600)
+    .attr('width', 600);
+
+// Append shapes to SVG container
+svg.append('rect')
+    .attr('width', 200)
+    .attr('height', 100)
+    .attr('fill', 'lightblue')
+    .attr('x', 20)
+    .attr('y', 20);
+svg.append('circle')
+    .attr('r', 50)
+    .attr('cx', 300)
+    .attr('cy', 70)
+    .attr('fill', 'pink');
+svg.append('line')
+    . attr('x1', 370)
+    .attr('x2', 400)
+    .attr('y1', 20)
+    .attr('y2', 120)
+    .attr('stroke', 'red');
+
+
+
+console.log(a, b);
