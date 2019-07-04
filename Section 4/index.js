e2fe9b9fea66a960a8ff98f2700f2d80597dd747
htrivedi04 <h.trivedi04@gmail.com>
2019-07-04 11:51:11 +1000

    Adding Groups and Margins

--- Section 4/index.js
+++ Section 4/index.js
@@ -1,4 +1,17 @@
-const svg = d3.select('svg');
+// Appending the svg directly to canvas
+const svg = d3.select('.canvas')
+                .append('svg')
+                .attr('width', 600)
+                .attr('height', 600);
+// Creating margins and graph (group) dimesions
+const margin = {top:20, right:20, bottom: 100, left: 100}
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+                    .attr('width', graphWidth)
+                    .attr('height', graphHeight)
+                    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 d3.json('menu.json').then(data => {
     // Making a linear y-scale
@@ -16,7 +29,7 @@ d3.json('menu.json').then(data => {
     // alert(data.map(item => item.name));
 
     // Join the data to rect
-    const rects = svg.selectAll('rect')
+    const rects = graph.selectAll('rect')
                         .data(data)
     rects.attr('width', x.bandwidth)
             .attr('height', d => y(d.orders))
