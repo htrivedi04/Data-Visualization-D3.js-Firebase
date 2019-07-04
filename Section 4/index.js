b414b0cb1d122033868b9adb04a577d531e4b986
htrivedi04 <h.trivedi04@gmail.com>
2019-07-04 11:34:02 +1000

    X & Y Scaling in Bar Charts

---
+++ Section 4/index.js
@@ -0,0 +1,36 @@
+const svg = d3.select('svg');
+
+d3.json('menu.json').then(data => {
+    // Making a linear y-scale
+    const y = d3.scaleLinear()
+                    .domain([0, 1000])
+                    .range([0, 500]);
+    
+    // Making a band scale for x-scale
+    const x = d3.scaleBand()
+                .domain(data.map(item => item.name))
+                .range([0, 500])
+                .paddingInner(0.25)
+                .paddingOuter(0.25);
+
+    // alert(data.map(item => item.name));
+
+    // Join the data to rect
+    const rects = svg.selectAll('rect')
+                        .data(data)
+    rects.attr('width', x.bandwidth)
+            .attr('height', d => y(d.orders))
+            .attr('fill', 'orange')
+            .attr('x', d => x(d.name))
+            .attr('y', d => 550 - y(d.orders));
+
+    // Append the enter selection to the DOM
+    rects.enter()
+            .append('rect')
+            .attr('width', x.bandwidth)
+            .attr('height', d => y(d.orders))
+            .attr('fill', 'orange')
+            .attr('x', d => x(d.name))
+            .attr('y', d => 550 - y(d.orders));
+    
+})
